fb1d459b7dde03e915652dd673040878e0849ac5
Sebastián Rodríguez Torres <104276355+SeRaTo12@users.noreply.github.com>
2025-01-29 19:06:46 -0600

    Update exercises_01.py

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -23,12 +23,12 @@ def sumar(a, b):
 # 3. Usar un bucle for para imprimir los números del 1 al 10
 # reemplaza la palabra reservada pass con tu código
 def bucle_for():
-    for i in range(1,11):
-        print(i)
+    for x in range(1,11):
+        print(x)
 
 
 # 5. Usar un bucle para imprimir cada número
 def print_numbers():
     numbers = [1, 2, 3, 4, 5]
-    for i in numbers:
-        print(i)
+    for x in numbers:
+        print(x)
