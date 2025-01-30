2190341563919054145b45449dff4f7f2b2a4e7f
Ancestral_Sen <143678672+AdolfoGuerrero@users.noreply.github.com>
2025-01-29 19:14:40 -0600

    177156-v2

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -5,27 +5,29 @@
 # orden apellido paterno, apellido materno y nombres, ejemplo: Revilla Silva Jesús Alberto
 # comienza cada nombre con mayúscuala y el resto con minúsculas incluye acentos y otros
 # caracteres del Español.
-# Escribe el código aquí
+nombre = "Guerrero González Diego Adolfo"
 
 
 
 # 1.1 Declara una variable matricula y asigna tu número de matrícula como entero
-# Escribe el código aquí
+matricula = 177156
 
 
 
 # 2. Crear una función que sume dos números
 def sumar(a, b):
-    pass # reemplaza pass con tu código
+    return a + b # reemplaza pass con tu código
 
 
 # 3. Usar un bucle for para imprimir los números del 1 al 10
 # reemplaza la palabra reservada pass con tu código
 def bucle_for():
-    pass
+    for i in range(1, 11):
+        print(i)
 
 
 # 5. Usar un bucle para imprimir cada número
 def print_numbers():
     numbers = [1, 2, 3, 4, 5]
-    # Escribe el código aquí
+    for number in numbers:
+        print(number)
