b04ac45081173a45dfc15a993434ceed3ecb12c1
josej9 <159069337+josej9@users.noreply.github.com>
2025-01-29 19:20:18 -0600

    Update exercises_01.py

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -7,25 +7,27 @@
 # caracteres del Español.
 # Escribe el código aquí
 
-
+nombre = 'Del Angel Rodríguez José Jess'
 
 # 1.1 Declara una variable matricula y asigna tu número de matrícula como entero
 # Escribe el código aquí
-
+matricula = 175800
 
 
 # 2. Crear una función que sume dos números
 def sumar(a, b):
-    pass # reemplaza pass con tu código
+    return a + b
 
 
 # 3. Usar un bucle for para imprimir los números del 1 al 10
 # reemplaza la palabra reservada pass con tu código
 def bucle_for():
-    pass
+    for i in range (1,11):
+        print(i)
 
 
 # 5. Usar un bucle para imprimir cada número
 def print_numbers():
     numbers = [1, 2, 3, 4, 5]
-    # Escribe el código aquí
+    for i in numbers:
+        print(i)
