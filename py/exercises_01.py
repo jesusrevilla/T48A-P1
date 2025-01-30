9b21d76ca277e14a7bc77b40aae1f3a411d36902
179598 <112294586+179598@users.noreply.github.com>
2025-01-29 19:11:03 -0600

    Update exercises_01.py

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -21,7 +21,7 @@ def sumar(a, b):
 # 3. Usar un bucle for para imprimir los números del 1 al 10
 # reemplaza la palabra reservada pass con tu código
 def bucle_for():
-    for i in range (1,10)
+    for i in range (1,10):
         print (i)
 
 
